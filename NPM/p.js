bb3884dda65018c8a05579eb396ebd06c11782a4
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-07 23:17:31 +0530

    exports into different file or directory/folder

--- NPM/p.js
+++ NPM/p.js
@@ -1,3 +1,6 @@
 const person1 = require('./person1');
 const person2 = require('./person2');
 const person3 = require('./person3');
+
+const  people = [person1, person2, person3];
+console.log(people);
