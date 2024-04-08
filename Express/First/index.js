61ac0762ace35849075b7763dba14adacaf7b7e3
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-08 12:23:38 +0530

    string  that contain html tag as a response

--- Express/First/index.js
+++ Express/First/index.js
@@ -3,7 +3,8 @@ const app =  express();
 app.use((req,res) => {
     console.log("We have a new request!");
     //res.send("Hello we got a request, this a response!")
-    res.send({color: 'red'});
+    //res.send({color: 'red'});
+    res.send('<h1>This is my webpage!</h1>');
 })
 
 app.listen(3000,() => {
