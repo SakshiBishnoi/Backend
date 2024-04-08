c3b799e650a684e649d5262e2667e0ee65c95f9e
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-08 19:06:08 +0530

    query string

--- Express/First/index.js
+++ Express/First/index.js
@@ -7,14 +7,44 @@ const app =  express();
 //     res.send('<h1>This is my webpage!</h1>');
 // })
 
-app.get('/cookies', (req, res) => {
-    console.log('cookies request!!!');
-    res.send('Cookie');
+app.get('/', (req, res) => {
+    res.send('This is homepage');
 });
 app.get('/cats', (req, res) => {
-    console.log('cats request!!!');
     res.send('Cats');
 });
+app.get('/cookies', (req, res) => {   
+    res.send('Cookie');
+});
+
+app.post('/cookies', (req, res) => {    
+    res.send('Its a post reques');
+});
+app.get('/search', (req, res) => {  
+    const {q} = req.query; 
+    if(!q){
+        return res.send('Please provide a search term')
+    }
+    console.log('Search query', req.query); 
+    res.send(`Search query: ${q}`); 
+});
+
+
+// app.get('/r/:subreddit', (req, res) => {
+//     console.log(req.params);
+//     const {subreddit} = req.params;
+//     res.send(`<h1>Browsing the ${subreddit} subreddit.</h1>`);
+// });
+// app.get('/r/:subreddit/:postId', (req, res) => {
+//     const {subreddit, postId} = req.params;
+//     res.send(`<h1>Viewing Post Id : ${postId} on ${subreddit} subreddit.</h1>`);
+// });
+
+
+app.get('*', (req, res) => {    
+    res.send('I do not know this path');
+});
+
 
 app.listen(3000,() => {
     console.log("Server is running");
