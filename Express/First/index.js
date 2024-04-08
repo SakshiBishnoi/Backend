e1780e38d9795d39da9690753908b7e288b779e8
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-08 16:40:49 +0530

    app.get multiple

--- Express/First/index.js
+++ Express/First/index.js
@@ -1,11 +1,20 @@
 const express =  require('express');
 const app =  express();
-app.use((req,res) => {
-    console.log("We have a new request!");
-    //res.send("Hello we got a request, this a response!")
-    //res.send({color: 'red'});
-    res.send('<h1>This is my webpage!</h1>');
-})
+// app.use((req,res) => {
+//     console.log("We have a new request!");
+//     //res.send("Hello we got a request, this a response!")
+//     //res.send({color: 'red'});
+//     res.send('<h1>This is my webpage!</h1>');
+// })
+
+app.get('/cookies', (req, res) => {
+    console.log('cookies request!!!');
+    res.send('Cookie');
+});
+app.get('/cats', (req, res) => {
+    console.log('cookies request!!!');
+    res.send('Cats');
+});
 
 app.listen(3000,() => {
     console.log("Server is running");
