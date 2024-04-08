4b20f0e650ad7ab127c0ebd389c7c7ff69ca1b17
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-08 11:33:55 +0530

    Started a server and recieved an incoming request

--- Express/First/index.js
+++ Express/First/index.js
@@ -1,1 +1,9 @@
 const express =  require('express');
+const app =  express();
+app.use( () => {
+    console.log("We have a new request!");
+})
+
+app.listen(3000,() => {
+    console.log("Server is running");
+});
