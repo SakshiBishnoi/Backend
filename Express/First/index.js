0d541fcf95002b6c0733d9b530d19ed081cf8527
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-08 12:17:50 +0530

    setting javascript object as response

--- Express/First/index.js
+++ Express/First/index.js
@@ -2,7 +2,8 @@ const express =  require('express');
 const app =  express();
 app.use((req,res) => {
     console.log("We have a new request!");
-    res.send("Hello we got a request, this a response!")
+    //res.send("Hello we got a request, this a response!")
+    res.send({color: 'red'});
 })
 
 app.listen(3000,() => {
