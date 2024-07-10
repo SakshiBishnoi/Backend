3cde047f7c35d2babf856785609d7c95f7f5af3d
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-07-10 13:36:37 +0530

    .

--- Rest/indexC.js
+++ Rest/indexC.js
@@ -1,9 +1,3 @@
-// GET /comments - list all comments
-// POST /comments - Create a new comment
-// GET /comments/:id Get one comment (using ID)
-// PATCH /comments/:id - Update one comment
-// DELETE /comments/:id Destroy one comment
-
 const express = require('express');
 const app = express();
 const path = require('path');
